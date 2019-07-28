234b9c546143e1d0c094ff2e0418024ce2d182b5
keepthethink <keepthethink@hotmail.com>
2019-07-28 22:18:18 +0800

    Init

---
+++ P1001.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Helium on 2019-07-28.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Luogu!" << std::endl;
+
+    return 0;
+}
